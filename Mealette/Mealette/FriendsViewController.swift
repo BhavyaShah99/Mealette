68fbf2458f50d3af8869dad13a1490c24e1bdd71
Bhavya Miten Shah <38031266+BhavyaShah99@users.noreply.github.com>
2020-01-06 19:32:55 -0500

    added a friends pop up view to show all users for users to add new friends and also animated its appearence

--- Mealette/Mealette/FriendsViewController.swift
+++ Mealette/Mealette/FriendsViewController.swift
@@ -10,19 +10,39 @@ import UIKit
 
 class FriendsViewController: UIViewController {
     
-    @IBOutlet var topnav: UINavigationItem!
+    @IBOutlet var topNav: UINavigationItem!
+    @IBOutlet var newFriendsTbl: UITableView!
+    @IBOutlet var existingFriendsTbl: UITableView!
+    @IBOutlet var newFriendsView: UIView!
+    @IBOutlet var searchExisting: UISearchBar!
+    @IBOutlet var searchNew: UISearchBar!
+   
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         setupView()
+        topNav.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addFriend))
     }
     
     func setupView() {
-        topnav.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addFriend))
+        newFriendsView.layer.cornerRadius = 7
     }
     
     @objc func addFriend() {
+        showNewFriendsView()
+    }
+    
+    func showNewFriendsView() {
+        self.view.addSubview(newFriendsView)
+        newFriendsView.center = self.view.center
         
+        newFriendsView.transform = CGAffineTransform.init(scaleX: 1.3, y: 1.3)
+        newFriendsView.alpha = 0
+        
+        UIView.animate(withDuration: 0.5) {
+            self.newFriendsView.alpha = 1
+            self.newFriendsView.transform = CGAffineTransform.identity
+        }
     }
 }
